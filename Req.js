7d42a510c59f51a5f97bdcf0f455983acf7a1dda
jsonlu <jsonlu@qq.com>
2017-08-18 17:31:35 +0800

    add multiple group config token

--- Req.js
+++ Req.js
@@ -5,53 +5,55 @@
 const axios = require('axios')
 const config = require('./config.json')
 axios.interceptors.request.use(function (config) {
-  return config;
+    return config;
 }, function (error) {
-  console.log(error)
-  return Promise.reject(error);
+    console.log(error)
+    return Promise.reject(error);
 });
 axios.interceptors.response.use(function (response) {
-  return response;
-  return Promise.reject(error);
+    return response;
+    return Promise.reject(error);
 });
 
 const instance = axios.create({
-  baseURL: 'https://oapi.dingtalk.com/robot/',
-  timeout: 8000,
-  headers: {'Content-Type': 'application/json; charset=utf-8'}
+    baseURL: 'https://oapi.dingtalk.com/robot/',
+    timeout: 8000,
+    headers: {'Content-Type': 'application/json; charset=utf-8'}
 });
 
 module.exports = {
-  req: function (url, param, call) {
-    instance.post(url, param)
-        .then(function (response) {
-          console.log(response.data);
-        })
-  },
-  reqDingd: function (param) {
-    let data = '项目:' + param.project.name + '\n团队:' + param.project.namespace + '\n提交:' + param.user_name + '\n分支:' +
-        param.ref.split("refs/heads/")[1] + '\n内容:'
-    let commit = param.commits
-    for (let l in commit) {
-      let comm = commit[l]
-      data += comm.message
-      if (comm.modified.length > 0) {
-        data += '\n修改:' + comm.modified
-      }
-      if (comm.added.length > 0) {
-        data += '\n增加:' + comm.added
-      }
-      if (comm.removed.length > 0) {
-        data += '\n移除:' + comm.removed
-      }
-      data += '\n详细:' + comm.url
+    req: function (url, param, call) {
+        instance.post(url, param)
+            .then(function (response) {
+                console.log('返回:' + JSON.stringify(response.data));
+            })
+    },
+    reqDingd: function (param) {
+        console.log('请求:' + JSON.stringify(param))
+        let data = '项目:' + param.project.name + '\n团队:' + param.project.namespace + '\n提交:' + param.user_name + '\n分支:' +
+            param.ref.split("refs/heads/")[1] + '\n内容:'
+        let commit = param.commits
+        for (let l in commit) {
+            let comm = commit[l]
+            data += comm.message
+            if (comm.modified.length > 0) {
+                data += '\n[修改]:' + comm.modified
+            }
+            if (comm.added.length > 0) {
+                data += '\n[增加]:' + comm.added
+            }
+            if (comm.removed.length > 0) {
+                data += '\n[移除]:' + comm.removed
+            }
+            data += '\n详细:' + comm.url
+        }
+        let p = {msgtype: 'text', text: {content: data}}
+        let groups = config.data.group
+        let team = param.project.namespace
+        let projects = groups[team]
+        if (typeof(projects) == "undefined")
+            return;
+        for (let key in projects)
+            this.req('send?access_token=' + projects[key], p, '')
     }
-    let p = {msgtype: 'text', text: {content: data}}
-    for (let index in config.data[param.project.name]) {
-      let key = config.data.FosunPay[index]
-      if (key == "")
-        return;
-      this.req('send?access_token=' + key, p, '')
-    }
-  }
 }
